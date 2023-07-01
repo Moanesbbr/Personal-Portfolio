1b289cc04285509a98d3950c7b49030fcea7d4a2
Ben Brahim Moanes <moanesbbr@gmail.com>
2023-07-01 08:11:54 +0100

    New Update

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -7,10 +7,24 @@ export const WorkData = [
 		site: "//media.licdn.com/dms/image/D4E2DAQGnvl3BMH4q5A/profile-treasury-image-shrink_1280_1280/0/1687666081627?e=1688450400&v=beta&t=0NZla1xCe-cYkvqpfFz4IRQA89WZa8ngUknSw-RNx5c",
 	},
 	{
+		title: "My Portfolio",
+		desc: "This personal portfolio .",
+		tech: ["Reactjs", "Framer motion"],
+		gitlink: "//github.com/Moanesbbr/Personal-Portfolio",
+		site: "//www.moanesbbr.me/",
+	},
+	{
 		title: "Food Delivery App",
 		desc: "A Cross-Platfrom mobile application for Food Delivery.",
 		tech: ["Flutter", "Firebase"],
 		gitlink: "//github.com/Moanesbbr/Food_Delivery",
 		site: "//github.com/Moanesbbr/Food_Delivery",
+	},
+	{
+		title: "CatsVsDogs",
+		desc: "Classifying Cats vs Dogs with a Convolutional Neural Network.",
+		tech: ["Python", "Jupyter Notebook"],
+		gitlink: "//github.com/Moanesbbr/catsVSdogs",
+		site: "//github.com/Moanesbbr/catsVSdogs",
 	},
 ];
