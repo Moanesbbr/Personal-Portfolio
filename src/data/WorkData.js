84083bad2eba92d8f732f12e45b0b7276842aa26
Ben Brahim Moanes <moanesbbr@gmail.com>
2023-07-06 08:36:15 +0100

    add new project

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -7,8 +7,15 @@ export const WorkData = [
 		site: "//media.licdn.com/dms/image/D4E2DAQGnvl3BMH4q5A/profile-treasury-image-shrink_1280_1280/0/1687666081627?e=1688450400&v=beta&t=0NZla1xCe-cYkvqpfFz4IRQA89WZa8ngUknSw-RNx5c",
 	},
 	{
+		title: "AppGPT",
+		desc: "AppGPT is a cross-platform mobile application that serves as a chatbot. it supports both text input and voice input.",
+		tech: ["Flutter"],
+		gitlink: "//github.com/Moanesbbr/AppGPT",
+		site: "//github.com/Moanesbbr/AppGPT",
+	},
+	{
 		title: "My Portfolio",
-		desc: "This personal portfolio .",
+		desc: "A personal website to show resume , projects and skills for client",
 		tech: ["Reactjs", "Framer motion"],
 		gitlink: "//github.com/Moanesbbr/Personal-Portfolio",
 		site: "//www.moanesbbr.me/",
